79e39d0d6ca92f8560594ea8a84df43a7a5c0d1d
Adam <adam@haglund.dev>
2021-03-02 15:15:04 +0100

    add lintin

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    es6: true,
+    es2017: true,
+    es2020: true,
+    es2021: true,
+    browser: true,
+    node: true,
+  },
+  extends: [
+    "plugin:@beequeue/base",
+    "plugin:@beequeue/react",
+    "plugin:@beequeue/node",
+    "plugin:@beequeue/typescript",
+  ],
+  overrides: [
+    {
+      files: "!pages/**/*",
+      rules: {
+        "import/no-default-export": "error",
+      },
+    },
+  ],
+}
